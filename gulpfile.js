22b75b07295586bb376bd3bbf6d126703fb76604
Rob Bearman <robbear@hyperfine.com>
2016-08-18 18:00:45 +0000

    Add vulcanize

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,35 @@ const clean = require('gulp-clean');
 const polymerRename = require('polymer-rename');
 const fileRename = require('gulp-rename');
 const closureCompiler = require('google-closure-compiler').gulp();
+const vulcanize = require('gulp-vulcanize');
 
-gulp.task('extract-data-binding-expressions', ['clean'], function() {
-  let stream = gulp.src('paper-dropdown-menu.html') // Usually this will be the vulcanized file
+gulp.task('extract-data-binding-expressions', ['vulcanize'], function() {
+  let stream = gulp.src('./build/paper-dropdown-menu-test-vulcanized.html') // Usually this will be the vulcanized file
     .pipe(polymerRename.extract())
     .pipe(fileRename(function(filePath) {
       filePath.basename += '.template';
   }))
-  .pipe(gulp.dest('./build'));
+  .pipe(gulp.dest('./'));
   
   return stream;
 });
 
-gulp.task('build', ['clean', 'extract-data-binding-expressions', 'compile-js', 'update-html-template']);
+gulp.task('vulcanize', ['clean'], function() {
+  // BUGBUG: Depends on soft link paper-dropdown-menu set up in the bower_components folder
+  let stream = gulp.src('bower_components/paper-dropdown-menu/test/paper-dropdown-menu.html')
+  .pipe(vulcanize({
+    abspath: ''
+  }))
+  .pipe(fileRename('paper-dropdown-menu-test-vulcanized.html'))
+  .pipe(gulp.dest('build'));
+  
+  return stream;
+});
 
 gulp.task('compile-js', ['extract-data-binding-expressions'], function() {
   let stream = closureCompiler({
-    js: './build/paper-dropdown-menu.template.js',
-    js_output_file: 'paper-dropdown-menu.compiled.js',
+    js: './build/paper-dropdown-menu-test-vulcanized.template.js',
+    js_output_file: 'paper-dropdown-menu-test-vulcanized.compiled.js',
     compilation_level: 'ADVANCED',
     warning_level: 'VERBOSE',
     polymer_pass: true,
@@ -36,8 +47,8 @@ gulp.task('compile-js', ['extract-data-binding-expressions'], function() {
 });
 
 gulp.task('update-html-template', ['compile-js'], function() {
-  let stream = gulp.src('./paper-dropdown-menu.html') // Usually this will be the vulcanized file
-  .pipe(polymerRename.replace('./build/paper-dropdown-menu.compiled.js'))
+  let stream = gulp.src('./build/paper-dropdown-menu-test-vulcanized.html') // Usually this will be the vulcanized file
+  .pipe(polymerRename.replace('./build/paper-dropdown-menu-test-vulcanized.compiled.js'))
   .pipe(gulp.dest('./build'));
   
   return stream;
@@ -49,6 +60,8 @@ gulp.task('clean', function() {
   
   return stream;
 });
+
+gulp.task('build', ['clean', 'extract-data-binding-expressions', 'compile-js', 'update-html-template']);
 
 gulp.task('default', function() {
   console.log('gulp commands this project supports:\n');
